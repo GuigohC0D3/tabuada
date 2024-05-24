5d874b73f6948a5a96f62458878b7340a45ece4d
GuigohC0D3 <guilhermewerneckpereira11@hotmail.com>
2024-05-24 11:54:01 -0300

    Terminando JavaScript da Multiplication

--- js/script.js
+++ js/script.js
@@ -3,7 +3,37 @@ const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const multiplicationInput = document.querySelector("#multiplicator");
 
+const multiplicationTitle = document.querySelector(
+  "#multiplication-title span"
+);
+
+const multiplicationTable = document.querySelector(
+  "#multiplication-operations"
+);
+
 // Functions
+const createTable = (number, multiplicatorNumber) => {
+  multiplicationTable.innerHTML = "";
+
+  for (i = 1; i <= multiplicatorNumber; i++) {
+    const result = number * i;
+
+    const template = `<div class="row">
+        <div class="operation">${number} x ${i} =</div>
+        <div class="result">${result}</div>
+      </div>`;
+
+    const parser = new DOMParser();
+
+    const htmlTemplate = parser.parseFromString(template, "text/html");
+
+    const row = htmlTemplate.querySelector(".row");
+
+    multiplicationTable.appendChild(row);
+  }
+
+  multiplicationTitle.innerText = number;
+};
 
 // Eventos
 multiplicationForm.addEventListener("submit", (e) => {
@@ -13,7 +43,7 @@ multiplicationForm.addEventListener("submit", (e) => {
 
   const multiplicatorNumber = +multiplicationInput.value;
 
-  if (!multiplicationNumber || multiplicatorNumber) return;
+  if (!multiplicationNumber || !multiplicatorNumber) return;
 
-  console.log(multiplicationNumber, multiplicatorNumber);
+  createTable(multiplicationNumber, multiplicatorNumber);
 });
